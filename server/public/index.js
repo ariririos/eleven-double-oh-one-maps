7377b7a995f5bd058b257a9742bee35f445fd17b
Cristian Rios <cristian.rios0719@gmail.com>
2019-11-19 05:54:04 -0500

    Much smaller data files

--- server/public/index.js
+++ server/public/index.js
@@ -2,11 +2,50 @@
 let mapboxToken = "pk.eyJ1IjoicmlvYzA3MTkiLCJhIjoiY2sydTA3NmlsMWgydDNtbWJueDczNTVyYSJ9.OXt2qQjXDCMVpDZA5pf3gw";
 
 const propAliases = {
-    blkGrpMedianIncome: "nhgis0021_ds233_20175_2017_blck_grp_csv_AH1PE001",
-    blkGrpPerCapitaIncome: "nhgis0021_ds233_20175_2017_blck_grp_csv_AH2RE001",
-    blkRenterTenure: "nhgis0012_ds172_2010_block.csv.IFO004",
-    blkPop: "nhgis0019_ds172_2010_block.csv.H7V001"
-}
+    // block group aliases
+    blkGrpPop: "AHY1E001",
+    blkGrpMedianIncome: "AH1PE001",
+    blkGrpPerCapitaIncome: "AH2RE001",
+    blkGrpMedianRent: "AH5RE001",
+    blkGrpMedianRentPercentIncome: "AH5YE001",
+    blkGrpMeanROWWidths: "ROW_Width",
+    // block aliases
+    blkPop: "H7V001",
+    blkRenters: "IFF004",
+    blkFamilies: "H8C002",
+    blkMeanROWWidths: "ROW_Width",
+    blkTrees: "TreeCount",
+    blkLights: "LightCount"
+};
+
+const layerAliases = {
+    // block group data
+    centralMedianIncome: "Median household income in 2017 by block group, Central",
+    porterMedianIncome: "Median household income in 2017 by block group, Porter",
+    centralPerCapitaIncome: "Per capita household income in 2017 by block group, Central",
+    porterPerCapitaIncome: "Per capita household income in 2017 by block group, Porter",
+    centralMedianRent: "Median gross rent by block group, Central",
+    porterMedianRent: "Median gross rent by block group, Porter",
+    centralMedianRentPercentIncome: "Median gross rent as a % of annual household income by block group, Central",
+    porterMedianRentPercentIncome: "Median gross rent as a % of annual household income by block group, Porter",
+    centralPopBlockGroup: "Total population by block group, Central",
+    porterPopBlockGroup: "Total population by block group, Porter",
+    centralMeanROWWidthsBlockGroup: "Mean Right of Way (ROW) Widths by block group, Central",
+    porterMeanROWWidthsBlockGroup: "Mean Right of Way (ROW) Widths by block group, Porter",
+    // block data
+    centralPopBlock: "Total population by block, Central",
+    porterPopBlock: "Total population by block, Porter",
+    centralRenters: "Renter-occupied housing units per block, Central",
+    porterRenters: "Renter-occupied housing units per block, Porter",
+    centralFamilies: "Family households per block, Central",
+    porterFamilies: "Family households per block, Porter",
+    centralMeanROWWidthsBlock: "Mean Right of Way (ROW) Widths by block, Central",
+    porterMeanROWWidthsBlock: "Mean Right of Way (ROW) Widths by block, Porter",
+    centralTrees: "Trees per block, Central",
+    porterTrees: "Trees per block, Porter",
+    centralLights: "Streetlights per block, Central",
+    porterLights: "Streetlights per block, Porter"
+};
 
 let map = L.map('map', {
     center: [42.310201212240266, -71.10151872038841], // Boston
@@ -47,6 +86,7 @@ async function createChloroplethLayer(geojson, shortPropName, longPropName, colo
 
     let propValues = moddedGeojson.features.map(feat => featPropGetter(feat)).filter(x => x != null);
     let propClasses = jenks(propValues, 5);
+    console.log(shortPropName, propClasses);
     // FIXME: some sort of off-by-one error in the coloring here
     let getColorForPolygon = v => {
         let colorBracket = 0;
@@ -123,83 +163,191 @@ async function createAndAddPointLayer(jsonPath, color, project, filterFn, addToM
     return point;
 }
 
-// function isMarkerInsidePolygon(marker, poly) {
-//     let polyPoints = poly.getLatLngs();
-//     let x = marker.getLatLng().lat, y = marker.getLatLng().lng;
-
-//     let inside = false;
-//     for (let i = 0, j = polyPoints.length - 1; i < polyPoints.length; j = i++) {
-//         let xi = polyPoints[i].lat, yi = polyPoints[i].lng;
-//         let xj = polyPoints[j].lat, yj = polyPoints[j].lng;
-
-//         let intersect = ((yi > y) != (yj > y))
-//             && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-//         if (intersect) inside = !inside;
-//     }
-
-//     return inside;
-// }
-
 async function main() {
-    const layerAliases = {
-        blkGrpMedianIncome: "Median household income in by block group, Cambridge and Somerville",
-        // centralMedianIncome: "Median household income in 2017 by block, Central",
-        // porterMedianIncome: "Median household income in 2017 by block, Porter",
-        trees: "(some) Trees",
-        centralPop: "Total population, Central",
-        porterPop: "Total population, Porter",
-        centralRenters: "Renter-occupied housing units per block, Central",
-        porterRenters: "Renter-occupied housing units per block, Porter"
-    }
 
     let chloroLayersData = {
-        blkGrpMedianIncome: ['data/MA_blck_grp_2017_intersect.geojson', propAliases.blkGrpMedianIncome, 'Median income', 'Greys', false, false],
-        // centralMedianIncome: ['data/CentralSelection.geojson', propAliases.blkRenterTenure, 'Median income', 'RdPu', true, true],
-        // porterMedianIncome: ['data/PorterSelection.geojson', propAliases.blkRenterTenure, 'Median income', 'RdPu', true, true],
-        centralPop: ['data/CentralSelection.geojson', propAliases.blkPop, 'Population', 'RdPu', true, false],
-        porterPop: ['data/PorterSelection.geojson', propAliases.blkPop, 'Population', 'RdPu', true, false],
-        centralRenters: ['data/CentralSelection.geojson', propAliases.blkRenterTenure, 'Renter-occupied housing units', 'PuBu', true, true],
-        porterRenters: ['data/PorterSelection.geojson', propAliases.blkRenterTenure, 'Renter-occupied housing units', 'PuBu', true, true]
+        'data/combo/MA_blck_grp_2017_Central.geojson': {
+            [propAliases.blkGrpPop]: {
+                legendName: layerAliases.centralPopBlockGroup,
+                longName: 'Total population',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianIncome]: {
+                legendName: layerAliases.centralMedianIncome,
+                longName: 'Median income',
+                addToMap: false
+            },
+            [propAliases.blkGrpPerCapitaIncome]: {
+                legendName: layerAliases.centralPerCapitaIncome,
+                longName: 'Per-capita income',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianRent]: {
+                legendName: layerAliases.centralMedianRent,
+                longName: 'Median gross rent',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianRentPercentIncome]: {
+                legendName: layerAliases.centralMedianRentPercentIncome,
+                longName: 'Median gross rent as a % of income',
+                addToMap: false
+            },
+            [propAliases.blkGrpMeanROWWidths]: {
+                legendName: layerAliases.centralMeanROWWidthsBlockGroup,
+                longName: 'Mean ROW width',
+                addToMap: false,
+                colorScheme: 'YlGn'
+            }
+        },
+        'data/combo/MA_blck_grp_2017_Porter.geojson': {
+            [propAliases.blkGrpPop]: {
+                legendName: layerAliases.porterPopBlockGroup,
+                longName: 'Total population',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianIncome]: {
+                legendName: layerAliases.porterMedianIncome,
+                longName: 'Median income',
+                addToMap: false
+            },
+            [propAliases.blkGrpPerCapitaIncome]: {
+                legendName: layerAliases.porterPerCapitaIncome,
+                longName: 'Per-capita income',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianRent]: {
+                legendName: layerAliases.porterMedianRent,
+                longName: 'Median gross rent',
+                addToMap: false
+            },
+            [propAliases.blkGrpMedianRentPercentIncome]: {
+                legendName: layerAliases.porterMedianRentPercentIncome,
+                longName: 'Median gross rent as a % of income',
+                addToMap: false
+            },
+            [propAliases.blkGrpMeanROWWidths]: {
+                legendName: layerAliases.porterMeanROWWidthsBlockGroup,
+                longName: 'Mean ROW width',
+                addToMap: false,
+                colorScheme: 'YlGn'
+            }
+        },
+        'data/combo/CentralSelection.geojson': {
+            [propAliases.blkPop]: {
+                legendName: layerAliases.centralPopBlock,
+                longName: 'Total population',
+                addToMap: false
+            },
+            [propAliases.blkRenters]: {
+                legendName: layerAliases.centralRenters,
+                longName: 'Renter-occupied housing units',
+                addToMap: true
+            },
+            [propAliases.blkFamilies]: {
+                legendName: layerAliases.centralFamilies,
+                longName: 'Family households',
+                addToMap: false
+            },
+            [propAliases.blkTrees]: {
+                legendName: layerAliases.centralTrees,
+                longName: 'Trees',
+                addToMap: false
+            },
+            [propAliases.blkLights]: {
+                legendName: layerAliases.centralLights,
+                longName: 'Streetlights',
+                addToMap: false
+            },
+            [propAliases.blkMeanROWWidths]: {
+                legendName: layerAliases.centralMeanROWWidthsBlock,
+                longName: 'Mean ROW width',
+                addToMap: false,
+                colorScheme: 'YlGn'
+            }
+        },
+        'data/combo/PorterSelection.geojson': {
+            [propAliases.blkPop]: {
+                legendName: layerAliases.porterPopBlock,
+                longName: 'Total population',
+                addToMap: false
+            },
+            [propAliases.blkRenters]: {
+                legendName: layerAliases.porterRenters,
+                longName: 'Renter-occupied housing units',
+                addToMap: true
+            },
+            [propAliases.blkFamilies]: {
+                legendName: layerAliases.porterFamilies,
+                longName: 'Family households',
+                addToMap: false
+            },
+            [propAliases.blkTrees]: {
+                legendName: layerAliases.porterTrees,
+                longName: 'Trees',
+                addToMap: false
+            },
+            [propAliases.blkLights]: {
+                legendName: layerAliases.porterLights,
+                longName: 'Streetlights',
+                addToMap: false
+            },
+            [propAliases.blkMeanROWWidths]: {
+                legendName: layerAliases.porterMeanROWWidthsBlock,
+                longName: 'Mean ROW width',
+                addToMap: false,
+                colorScheme: 'YlGn'
+            }
+        }
     };
 
     let chloroLayers = Object.assign({}, Object.fromEntries(Object.keys(chloroLayersData).map(key => [key])));
 
-    for (let [name, data] of Object.entries(chloroLayersData)) {
-        chloroLayers[name] = await createAndAddChloroplethLayer(...data, map);
+    for (let [path, layers] of Object.entries(chloroLayersData)) {
+        chloroLayers[path] = {};
+        for (let [layerPropName, layerOpts] of Object.entries(layers)) {
+            chloroLayers[path][layerOpts.legendName] = await createAndAddChloroplethLayer(
+                                                                path,
+                                                                layerPropName,
+                                                                layerOpts.longName,
+                                                                layerOpts.colorScheme || 'RdPu',
+                                                                false,
+                                                                layerOpts.addToMap,
+                                                                map
+                                                             );
+        }
     }
 
     // FIXME: filter trees
-    let pointLayersData = {
-        trees: ['data/ENVIRONMENTAL_StreetTrees.geojson', 'green', false, ((p, i) => i % 500 == 0), true]
-    };
+    // let pointLayersData = {
+    //     // trees: ['data/ENVIRONMENTAL_StreetTrees.geojson', 'green', false, ((p, i) => i % 500 == 0), true]
+    // };
 
-    let pointLayers = Object.assign({}, Object.fromEntries(Object.keys(pointLayersData).map(key => [key])));
+    // let pointLayers = Object.assign({}, Object.fromEntries(Object.keys(pointLayersData).map(key => [key])));
 
-    for (let [name, data] of Object.entries(pointLayersData)) {
-        pointLayers[name] = await createAndAddPointLayer(...data, map);
-    }
+    // for (let [name, data] of Object.entries(pointLayersData)) {
+    //     pointLayers[name] = await createAndAddPointLayer(...data, map);
+    // }
 
     let baseMap = L.layerGroup();
     baseMap.addTo(map);
 
     L.control.scale().addTo(map);
 
+    let overlays = {};
+
+    for (let path of Object.values(chloroLayers)) {
+        for (let [legendName, layerData] of Object.entries(path)) {
+            overlays[legendName] = layerData.layer;
+        }
+    }
+
     L.control.layers(
         // basemaps:
         {
             "Base map only": baseMap,
-            [layerAliases.blkGrpMedianIncome]: chloroLayers.blkGrpMedianIncome.layer
+            // [layerAliases.blkGrpMedianIncome]: chloroLayers.blkGrpMedianIncomeCentral.layer
         },
-        // overlays:
-        {
-            // [layerAliases.centralMedianIncome]: chloroLayers.centralMedianIncome.layer,
-            // [layerAliases.porterMedianIncome]: chloroLayers.porterMedianIncome.layer,
-            [layerAliases.trees]: pointLayers.trees.layer,
-            [layerAliases.centralPop]: chloroLayers.centralPop.layer,
-            [layerAliases.porterPop]: chloroLayers.porterPop.layer,
-            [layerAliases.centralRenters]: chloroLayers.centralRenters.layer,
-            [layerAliases.porterRenters]: chloroLayers.porterRenters.layer,
-        }
+        overlays
     ).addTo(map);
 
     // FIXME: legends for base layers
@@ -209,64 +357,66 @@ async function main() {
 
     // Legends per layer:
 
-    let legends = Object.assign({}, Object.fromEntries(Object.keys(chloroLayersData).map(key => [key])), Object.fromEntries(Object.keys(pointLayersData).map(key => [key])));
+    let legends = Object.assign({}, Object.fromEntries(Object.keys(chloroLayersData).map(key => [key])));//, Object.fromEntries(Object.keys(pointLayersData).map(key => [key])));
 
     // Chloropleth legends
     // eslint-disable-next-line require-atomic-updates
-    for (let [name, chloro] of Object.entries(chloroLayers)) {
-        let legend = L.control({ position: 'bottomright' });
-        legend.onAdd = () => {
-            let div = L.DomUtil.create('div', 'info legend');
-            div.onclick = () => {
-                map.flyToBounds(chloro.layer.getBounds());
+    for (let path of Object.values(chloroLayers)) {
+        for (let [legendName, layerData] of Object.entries(path)) {
+            let legend = L.control({ position: 'bottomright' });
+            legend.onAdd = () => {
+                let div = L.DomUtil.create('div', 'info legend');
+                div.onclick = () => {
+                    map.flyToBounds(layerData.layer.getBounds());
+                }
+                let colors = layerData.colors;
+                div.innerHTML += `<div>${legendName}<br/></div>`;
+                for (let i = 1; i < colors.propClasses.length; i++) {
+                    div.innerHTML += `<div><i style="background: ${colors.getColorForPolygon(colors.propClasses[i])}"></i>${colors.propClasses[i]}${colors.propClasses[i+1] ? "-" + colors.propClasses[i+1] + "<br>": "+"}</div>`.trim();
+                }
+                return div;
             }
-            let colors = chloro.colors;
-            div.innerHTML += `<div>${layerAliases[name]}<br/></div>`;
-            for (let i = 1; i < colors.propClasses.length; i++) {
-                div.innerHTML += `<div><i style="background: ${colors.getColorForPolygon(colors.propClasses[i])}"></i>${colors.propClasses[i]}${colors.propClasses[i+1] ? "-" + colors.propClasses[i+1] + "<br>": "+"}</div>`.trim();
-            }
-            return div;
+            legends[legendName] = legend;
         }
-        legends[name] = legend;
     }
 
     // Point layer legends
     // eslint-disable-next-line require-atomic-updates
-    for (let [name, point] of Object.entries(pointLayers)) {
-        let legend = L.control({ position: 'bottomright' });
-        legend.onAdd = () => {
-            let div = L.DomUtil.create('div', 'info legend');
-            div.onclick = () => {
-                map.flyToBounds(point.layer.getBounds());
-            }
-            div.innerHTML += `<div><i style="background: ${point.color}"></i>${layerAliases[name]}</div>`.trim();
-            return div;
-        }
-        legends[name] = legend;
-    }
+    // for (let [name, point] of Object.entries(pointLayers)) {
+    //     let legend = L.control({ position: 'bottomright' });
+    //     legend.onAdd = () => {
+    //         let div = L.DomUtil.create('div', 'info legend');
+    //         div.onclick = () => {
+    //             map.flyToBounds(point.layer.getBounds());
+    //         }
+    //         div.innerHTML += `<div><i style="background: ${point.color}"></i>${layerAliases[name]}</div>`.trim();
+    //         return div;
+    //     }
+    //     legends[name] = legend;
+    // }
 
     // Enable legends for all layers active at start
-    for (let [name, data] of Object.entries(chloroLayersData)) {
-        if (data.slice(-1)[0]) { // if map displayed at start
-            legends[name].addTo(map);
+    for (let path of Object.values(chloroLayersData)) {
+        for (let [_, data] of Object.entries(path)) {
+            if (data.addToMap) { // if map displayed at start
+                legends[data.legendName].addTo(map);
+            }
         }
     }
 
-    for (let [name, data] of Object.entries(pointLayersData)) {
-        if (data.slice(-1)[0]) { // if map displayed at start
-            legends[name].addTo(map);
-        }
-    }
+    // for (let [name, data] of Object.entries(pointLayersData)) {
+    //     if (data.slice(-1)[0]) { // if map displayed at start
+    //         legends[name].addTo(map);
+    //     }
+    // }
 
     map.on('overlayadd', function(e) {
         map.flyToBounds(e.layer.getBounds());
-        let shortName = Object.entries(layerAliases).find(([k, v]) => v === e.name)[0];
-        legends[shortName].addTo(this);
+        legends[e.name].addTo(this);
     });
 
     map.on('overlayremove', function(e) {
-        let shortName = Object.entries(layerAliases).find(([k, v]) => v === e.name)[0];
-        this.removeControl(legends[shortName]);
+        this.removeControl(legends[e.name]);
     });
 
     map.flyTo([42.37539336229674, -71.11111767590047], 14, { duration: 3 }); // over Cambridge
